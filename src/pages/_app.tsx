a25077b8a7637280856ab1f4630601acf6d66aa4
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-02-28 16:36:35 +0100

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ContentfulLivePreviewProvider
+      enableInspectorMode
+      enableLiveUpdates
+      targetOrigin="http://localhost:3001"
+      locale={"en-US"}
+    >
+      <Component {...pageProps} />
+    </ContentfulLivePreviewProvider>
+  );
 }
